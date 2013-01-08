c821b5db3fd4933aa46ee2fd52cd5a9b62218406
Sandra Lombardi <sandra.f.lombardi@gmail.com>
2013-01-08 17:19:30 -0500

    Datamaps Changes

--- app/views.py
+++ app/views.py
@@ -5,16 +5,18 @@ from httplib import *
 
 import twitter, urllib2, oauth2
 from etsy import Etsy, EtsyEnvSandbox, EtsyEnvProduction
+from geopy import geocoders
+
 
 from app import app
 import pdb
 
 ### API IDs ####
-Twitter_API_ID = 2367105
 Etsy_API_ID = '66dg8mzhdt2e31nfmtpn2rna'
 
 ### Default Vars ###
 etsy_env= EtsyEnvProduction()
+gn = geocoders.GeoNames()
 
 ### Main ###
 def search_etsy_tweets():
@@ -23,9 +25,14 @@ def search_etsy_tweets():
     tweets = api.GetSearch(searchTerm, include_entities = 1, result_type = "recent", per_page = 5)
     results = []
     for tweet in tweets:
+        pdb.set_trace()
+        twitter_user = tweet.GetUser()
+        twitter_user_data = api.GetUser(twitter_user.screen_name)
+        twitter_user_loc = gn.geocode(twitter_user_data.loc.encode('utf-8'), exactly_one=False)[0]
         tweet_urls = get_urls(tweet)
-        tweet_location = get_location(tweet)
-        tweet_retweets = get_retweets(tweet)
+        #tweet_retweets = get_retweets(tweet)
+        tweet_retweets = 1
+        tweet_location = 3
         for item in tweet_urls:
             expanded_url = get_expanded_url(item)
             if expanded_url.find("http://www.etsy.com/listing/") != -1:
@@ -33,7 +40,7 @@ def search_etsy_tweets():
                 end_pos = expanded_url.find("/",orig_pos)
                 listing_id = expanded_url[orig_pos:end_pos]
                 listing_data = get_listing(listing_id)
-                json_listing = format_json_data(listing_data[0])
+                json_listing = format_json_data(listing_data[0], tweet_location, tweet_retweets)
                 results.append(json_listing)
     return results
 
@@ -44,12 +51,6 @@ def get_listing(listing_id):
 def get_urls(tweet):
     tweet_urls = []
     for url in tweet.urls:
-        tweet_urls.append(url.url)
-    return tweet_urls
-
-def get_location(tweet):
-    tweet_urls = []
-    for url in tweet.location:
         tweet_urls.append(url.url)
     return tweet_urls
 
@@ -66,8 +67,9 @@ def format_json_data(listing_data, tweet_location, tweet_retweets):
     listing_title = listing_data['title']
     listing_views = listing_data['views']
     listing_favorers = listing_data['num_favorers']
-    listing_score = get_score(listing_favorers, tweet_retweets, listing_views)
-
+    listing_id = listing_data['listing_id']
+    #listing_score = get_score(listing_favorers, tweet_retweets, listing_views)
+    listing_score = 100
     '''
      also need to use
 
@@ -75,16 +77,17 @@ def format_json_data(listing_data, tweet_location, tweet_retweets):
      need to pass listing_id and listing_image_id to get main image.
      
      '''
-    item_data = [{'listing_title': listing_title,
+    json_listing = {'listing_id' : listing_id,
+            'listing_title': listing_title,
             'views': listing_views,
             'favorers': listing_favorers,
             'tweet_location' : tweet_location,
             'retweets' : tweet_retweets,
             'score': listing_score
-            }]
+            }
 
-    json_listing = {'listing_id': listing_data['listing_id'],
-                          'listing_data': item_data }
+    #json_listing = {'listing_id': listing_data['listing_id'],
+                          #'listing_data': item_data }
     
     
     return json_listing
@@ -104,4 +107,7 @@ def get_expanded_url(url):
 @app.route('/')
 def display_data():
     data = search_etsy_tweets()
-    return render_template('index.html', data = data)
+    resp = make_response(render_template('index.html', data = data))
+    resp.cache_control.no_cache = True
+    return resp
+    #return render_template('index.html', data = data)
