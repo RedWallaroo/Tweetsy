64f59b88711f19511aea266e5489aa30c6c24c6e
Sandra Lombardi <sandra.f.lombardi@gmail.com>
2013-01-08 09:02:22 -0500

    Changes D3

--- app/views.py
+++ app/views.py
@@ -1,43 +1,31 @@
+import os, sys
+
 from flask import *
 from httplib import *
-from app import app
-import os
-import twitter
-import util
-import urllib2
-import pdb
-import webbrowser
-import os, sys 
-import oauth2
+
+import twitter, urllib2, oauth2
 from etsy import Etsy, EtsyEnvSandbox, EtsyEnvProduction
 
+from app import app
+import pdb
 
-API_ID = 2367105
+### API IDs ####
+Twitter_API_ID = 2367105
 Etsy_API_ID = '66dg8mzhdt2e31nfmtpn2rna'
-oauth_consumer_secret = 'kfteru58n3'
+
+### Default Vars ###
 etsy_env= EtsyEnvProduction()
 
-
-def get_listing(listing_id):
-
-    etsy_api = Etsy(api_key=Etsy_API_ID, etsy_env=etsy_env)
-    return etsy_api.getListing(listing_id=listing_id)
-
-
+### Main ###
 def search_etsy_tweets():
-    """
-    Print recent tweets containing `searchTerm`.
-    
-    we'll search for tweets containing 'etsy' and filter by links in tweets, we will
-    only care about the ones containing listing links.
-
-    """
     api = twitter.Api()
     searchTerm = "etsy"
     tweets = api.GetSearch(searchTerm, include_entities = 1, result_type = "recent", per_page = 5)
     results = []
     for tweet in tweets:
         tweet_urls = get_urls(tweet)
+        tweet_location = get_location(tweet)
+        tweet_retweets = get_retweets(tweet)
         for item in tweet_urls:
             expanded_url = get_expanded_url(item)
             if expanded_url.find("http://www.etsy.com/listing/") != -1:
@@ -45,10 +33,8 @@ def search_etsy_tweets():
                 end_pos = expanded_url.find("/",orig_pos)
                 listing_id = expanded_url[orig_pos:end_pos]
                 listing_data = get_listing(listing_id)
-                formatted_listing = format_listing_data(listing_data[0])
-                results.append(formatted_listing)
-    
-    #return jsonify(results)
+                json_listing = format_json_data(listing_data[0])
+                results.append(json_listing)
     return results
 
 def get_listing(listing_id):
@@ -61,13 +47,26 @@ def get_urls(tweet):
         tweet_urls.append(url.url)
     return tweet_urls
 
-def format_listing_data(listing_data):
+def get_location(tweet):
+    tweet_urls = []
+    for url in tweet.location:
+        tweet_urls.append(url.url)
+    return tweet_urls
+
+def get_retweets(tweet):
+    tweet_urls = []
+    for url in tweet.retweets:
+        tweet_urls.append(url.url)
+    return tweet_urls
+
+def get_Score(listing_favorers, tweet_retweets, listing_views):
+    return 10
+
+def format_json_data(listing_data, tweet_location, tweet_retweets):
     listing_title = listing_data['title']
     listing_views = listing_data['views']
     listing_favorers = listing_data['num_favorers']
-    tweet_location = ''
-    tweet_retweets = ''
-    listing_score = 10
+    listing_score = get_score(listing_favorers, tweet_retweets, listing_views)
 
     '''
      also need to use
@@ -76,20 +75,21 @@ def format_listing_data(listing_data):
      need to pass listing_id and listing_image_id to get main image.
      
      '''
-    item_data = {'listing_title': listing_title,
+    item_data = [{'listing_title': listing_title,
             'views': listing_views,
             'favorers': listing_favorers,
             'tweet_location' : tweet_location,
             'retweets' : tweet_retweets,
             'score': listing_score
-            }
+            }]
 
-    formatted_listing = {'listing_id': listing_data['listing_id'],
+    json_listing = {'listing_id': listing_data['listing_id'],
                           'listing_data': item_data }
     
     
-    return formatted_listing
-    
+    return json_listing
+
+### Helpers ###
 class HeadRequest(urllib2.Request):
     def get_method(self): return "HEAD"
      
@@ -100,7 +100,7 @@ def get_expanded_url(url):
     except:
         return "empty"
 
-
+### Flask ###
 @app.route('/')
 def display_data():
     data = search_etsy_tweets()
