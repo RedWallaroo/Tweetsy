ccf785df381bd9b98f2df02e146bc9e1fb181384
Sandra Lombardi <sandra.f.lombardi@gmail.com>
2013-01-09 00:35:25 -0500

    D3 changes

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ import os, sys
 from flask import *
 from httplib import *
 
-import twitter, urllib2, oauth2
+import twitter, urllib
 from etsy import Etsy, EtsyEnvSandbox, EtsyEnvProduction
 from geopy import geocoders
 
@@ -22,25 +22,19 @@ gn = geocoders.GeoNames()
 def search_etsy_tweets():
     api = twitter.Api()
     searchTerm = "etsy"
-    tweets = api.GetSearch(searchTerm, include_entities = 1, result_type = "recent", per_page = 5)
+    tweets = api.GetSearch(searchTerm, include_entities=1, result_type="recent", per_page=10, geocode=("34.0522342","-118.4911912","4000mi"))
     results = []
     for tweet in tweets:
-        pdb.set_trace()
-        twitter_user = tweet.GetUser()
-        twitter_user_data = api.GetUser(twitter_user.screen_name)
-        twitter_user_loc = gn.geocode(twitter_user_data.loc.encode('utf-8'), exactly_one=False)[0]
         tweet_urls = get_urls(tweet)
-        #tweet_retweets = get_retweets(tweet)
-        tweet_retweets = 1
-        tweet_location = 3
         for item in tweet_urls:
-            expanded_url = get_expanded_url(item)
+            expanded_url = get_expanded_url(item.encode('utf-8'))
             if expanded_url.find("http://www.etsy.com/listing/") != -1:
                 orig_pos = expanded_url.find("http://www.etsy.com/listing/") + 28
                 end_pos = expanded_url.find("/",orig_pos)
                 listing_id = expanded_url[orig_pos:end_pos]
                 listing_data = get_listing(listing_id)
-                json_listing = format_json_data(listing_data[0], tweet_location, tweet_retweets)
+                tweet_location = get_location(tweet)
+                json_listing = format_json_data(listing_data[0], tweet_location)
                 results.append(json_listing)
     return results
 
@@ -49,27 +43,34 @@ def get_listing(listing_id):
     return etsy_api.getListing(listing_id=listing_id)
 
 def get_urls(tweet):
-    tweet_urls = []
-    for url in tweet.urls:
-        tweet_urls.append(url.url)
+    tweet_urls = [url.expanded_url for url in tweet.urls]
     return tweet_urls
 
-def get_retweets(tweet):
-    tweet_urls = []
-    for url in tweet.retweets:
-        tweet_urls.append(url.url)
-    return tweet_urls
+def get_location(tweet):
+    pdb.set_trace()
+    if tweet.geo != None:
+        tweet_coordinates = tweet.get.coordinates
+    elif tweet.location != None:
+        tweet_geocode = gn.geocode(tweet.location, exactly_one=False)
+        tweet_coordinates = tweet_geocode[0][1]
+    else:
+        tweet_coordinates = ("34.0522342","-118.4911912")
 
-def get_Score(listing_favorers, tweet_retweets, listing_views):
-    return 10
+    return tweet_coordinates
 
-def format_json_data(listing_data, tweet_location, tweet_retweets):
+def get_expanded_url(url):
+    try:
+        resp = urllib.urlopen(url)
+        return resp.url
+    except:
+        return 'empty'
+
+def format_json_data(listing_data, tweet_location):
     listing_title = listing_data['title']
     listing_views = listing_data['views']
     listing_favorers = listing_data['num_favorers']
     listing_id = listing_data['listing_id']
-    #listing_score = get_score(listing_favorers, tweet_retweets, listing_views)
-    listing_score = 100
+    #listing_image = listing_data['listing']
     '''
      also need to use
 
@@ -77,31 +78,15 @@ def format_json_data(listing_data, tweet_location, tweet_retweets):
      need to pass listing_id and listing_image_id to get main image.
      
      '''
-    json_listing = {'listing_id' : listing_id,
-            'listing_title': listing_title,
+    json_listing = {'id' : listing_id,
+            'title': listing_title,
             'views': listing_views,
             'favorers': listing_favorers,
-            'tweet_location' : tweet_location,
-            'retweets' : tweet_retweets,
-            'score': listing_score
+            'latitude' : tweet_location[0],
+            'longitude': tweet_location[1],
             }
-
-    #json_listing = {'listing_id': listing_data['listing_id'],
-                          #'listing_data': item_data }
-    
     
     return json_listing
-
-### Helpers ###
-class HeadRequest(urllib2.Request):
-    def get_method(self): return "HEAD"
-     
-def get_expanded_url(url):
-    try:
-       expanded_url = urllib2.urlopen(HeadRequest(url))
-       return expanded_url.geturl()
-    except:
-        return "empty"
 
 ### Flask ###
 @app.route('/')
@@ -110,4 +95,3 @@ def display_data():
     resp = make_response(render_template('index.html', data = data))
     resp.cache_control.no_cache = True
     return resp
-    #return render_template('index.html', data = data)
