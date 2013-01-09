7f4d260ab9a9e56340093502dd40b7510786f5e0
Sandra Lombardi <sandra.f.lombardi@gmail.com>
2013-01-09 16:27:10 -0500

    Set Port back to default

--- app/__init__.py
+++ app/__init__.py
@@ -5,5 +5,5 @@ from app import views
 
 if __name__=='__main__':
 	#bind to port if defined, otherwise default
-	port = int(os.environ.get('PORT', 5000))
-	app.run(host='0.0.0.0', port=port)
+	#port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0')
